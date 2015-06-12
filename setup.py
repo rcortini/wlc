c4f7a74e9524286eb3bd4957cecad540cf464e5c
Ruggero Cortini <cortini@lptl.jussieu.fr>
2015-06-12 16:29:00 +0200

    added wlc_f_rho and renamed python module to pywlc

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from distutils.core import setup, Extension
 import subprocess
-module=Extension('wlc', ['wlc_python.c'])
+module=Extension('pywlc', ['wlc_python.c'])
 module.libraries = ['wlc']
-# module.extra_compile_args=[subprocess.check_output(["wlc-config", "--libs"]),
-                           # subprocess.check_output(["wlc-config", "--cflags"])];
 module.extra_compile_args=[subprocess.check_output(["wlc-config", "--cflags"])]
 module.extra_link_args=[subprocess.check_output(["wlc-config", "--libs"])]
-setup(name='wlc', version='0.0', ext_modules=[module])
+setup(name='pywlc', version='0.0', ext_modules=[module])
--- wlc_python.c
+++ wlc_python.c
@@ -1,26 +1,42 @@
 #include <Python.h>
 #include <wlc/wlc.h>
 
-static PyObject* g_F (PyObject* self, PyObject *args)
+// wlc_g_F
+static PyObject* pywlc_g_F (PyObject* self, PyObject *args)
 {
-  double F, lpb, g;
+  double F, lpb;
   if (!PyArg_ParseTuple(args, "dd", &F, &lpb))
     return NULL;
 
   // calculate wlc_g_F
-  g = wlc_g_F (F, lpb);
+  double g = wlc_g_F (F, lpb);
   return Py_BuildValue("d", g);
 }
 
-static char g_F_docs[] = "g_F: calculate Gibbs free energy as a function of force\n";
+static char pywlc_g_F_docs[] = "wlc_g_F: calculate Gibbs free energy as a function of force\n";
 
-/* now we initialize the module */
-static PyMethodDef wlc_funcs[] = {
-  {"g_F", (PyCFunction)g_F, METH_VARARGS, g_F_docs},
+// wlc_f_rho
+static PyObject* pywlc_f_rho (PyObject* self, PyObject *args)
+{
+  double rho, lpb;
+  if (!PyArg_ParseTuple(args, "dd", &rho, &lpb))
+    return NULL;
+
+  // calculate wlc_g_F
+  double f = wlc_f_rho (rho, lpb);
+  return Py_BuildValue("d", f);
+}
+
+static char pywlc_f_rho_docs[] = "wlc_f_rho: calculate Helmholtz free energy as a function of z/L\n";
+
+// now we initialize the module
+static PyMethodDef pywlc_funcs[] = {
+  {"wlc_g_F", (PyCFunction)pywlc_g_F, METH_VARARGS, pywlc_g_F_docs},
+  {"wlc_f_rho", (PyCFunction)pywlc_f_rho, METH_VARARGS, pywlc_f_rho_docs},
   {NULL}
 };
 
-void initwlc(void)
+void initpywlc(void)
 {
-  Py_InitModule3 ("wlc", wlc_funcs, "wlc module, wraps wlc C library");
+  Py_InitModule3 ("pywlc", pywlc_funcs, "pywlc module, wraps wlc C library");
 }
