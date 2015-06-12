934fcec5c9dcd4cb125cda93e55ba5a68e54ff06
Ruggero Cortini <cortini@lptl.jussieu.fr>
2015-06-12 16:36:40 +0200

    added all functions of the wlc "exact" regime

--- wlc_python.c
+++ wlc_python.c
@@ -29,10 +29,40 @@ static PyObject* pywlc_f_rho (PyObject* self, PyObject *args)
 
 static char pywlc_f_rho_docs[] = "wlc_f_rho: calculate Helmholtz free energy as a function of z/L\n";
 
+// wlc_F_rho
+static PyObject* pywlc_F_rho (PyObject* self, PyObject *args)
+{
+  double rho, lpb;
+  if (!PyArg_ParseTuple(args, "dd", &rho, &lpb))
+    return NULL;
+
+  // calculate wlc_g_F
+  double F = wlc_F_rho (rho, lpb);
+  return Py_BuildValue("d", F);
+}
+
+static char pywlc_F_rho_docs[] = "wlc_F_rho: calculate force as a function of z/L\n";
+
+// wlc_g_rho
+static PyObject* pywlc_g_rho (PyObject* self, PyObject *args)
+{
+  double rho, lpb;
+  if (!PyArg_ParseTuple(args, "dd", &rho, &lpb))
+    return NULL;
+
+  // calculate wlc_g_F
+  double g = wlc_g_rho (rho, lpb);
+  return Py_BuildValue("d", g);
+}
+
+static char pywlc_g_rho_docs[] = "wlc_g_rho: calculate Gibbs free energy as a function of z/L\n";
+
 // now we initialize the module
 static PyMethodDef pywlc_funcs[] = {
   {"wlc_g_F", (PyCFunction)pywlc_g_F, METH_VARARGS, pywlc_g_F_docs},
   {"wlc_f_rho", (PyCFunction)pywlc_f_rho, METH_VARARGS, pywlc_f_rho_docs},
+  {"wlc_F_rho", (PyCFunction)pywlc_F_rho, METH_VARARGS, pywlc_F_rho_docs},
+  {"wlc_g_rho", (PyCFunction)pywlc_g_rho, METH_VARARGS, pywlc_g_rho_docs},
   {NULL}
 };
 
